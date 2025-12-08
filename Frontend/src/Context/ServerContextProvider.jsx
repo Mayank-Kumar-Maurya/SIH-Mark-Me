585bd20ba7b3b526dcf08f1211e8ae56bab5a829
Mayank-Kumar-Maurya <mayankmaurya2122@gmail.com>
2025-12-08 13:57:27 +0530

    Added face context

--- Frontend/src/Context/ServerContextProvider.jsx
+++ Frontend/src/Context/ServerContextProvider.jsx
@@ -5,12 +5,35 @@ import ServerContext from './ServerContext.js';
 
 function ServerContextProvider({children}) {
 
+    let server = axios.create({
+        baseURL: "http://localhost:8070/api/v1",
+        withCredentials: true,
+    });
 
     let handleVideo = async()=>
     {
-        
+
     }
 
+
+    let sendFaceDescriptor = async()=>
+    {
+        let serres = await server.post("/sendFaceDescriptor",
+            {
+                faceDescriptor: ["hello"],
+                rollno: 23239
+            }
+        );
+
+        if(serres.status == 200)
+        {
+            console.log("send code res: ", serres.data);
+        }
+        else
+        {
+            console.log("false");
+        }
+    }
 
   return (
     <ServerContext.Provider value={{}}>
